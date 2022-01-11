f14348daa59a56b5b5ae03695189332de622afe8
Nina Turner <nturner@students.polytechnic.org>
2022-01-11 14:42:20 -0800

    dieRoll method added

--- Die.java
+++ Die.java
@@ -1,3 +1,9 @@
 public class Die{
-    
+
+    public int dieRoll(){ //rolls the dice
+        int firstRoll = (int)Math.random()*6 + 1;
+        int secondRoll = (int)Math.random()*6 + 1;
+        return firstRoll+secondRoll;
+    }
+
 }
