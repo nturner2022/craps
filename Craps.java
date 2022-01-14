a915f9db5ca349c26a55677ee325e1c7f65e24fd
Nina Turner <nturner@students.polytechnic.org>
2022-01-13 20:12:47 -0800

    Scanner to initiate Game

--- Craps.java
+++ Craps.java
@@ -1,3 +1,20 @@
+
+import java.util.Scanner;
+
 public class Craps {
     
+    public static void main(String args[]){
+        System.out.println("Welcome to the game Craps! Would you like to play? (Yes/No)");
+        Scanner in = new Scanner(System.in);
+        String initiateGame = in.next();
+        if (initiateGame.equalsIgnoreCase("Yes")){
+
+        }
+        else {
+            System.out.println("Okay! Have a nice day!");
+        }
+       
+        in.close();
+    }
+
 }
