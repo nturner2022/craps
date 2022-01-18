9185cb1673b23c564955c03bfdbcc62838080c27
Nina Turner <nturner@students.polytechnic.org>
2022-01-18 13:00:15 -0800

    Fixed small problems

--- Craps.java
+++ Craps.java
@@ -8,18 +8,24 @@ public class Craps {
         Scanner in = new Scanner(System.in);
         String initiateGame = in.next();
         Die dice = new Die();
-        System.out.println("Do you need the instructions? (Yes/No)");
-        String instructions = in.next();
-        if (instructions.equalsIgnoreCase("Yes")){
-            System.out.println(" 1. A player rolls two six-sided dice and adds the numbers rolled together.");
-            System.out.println("2. On this first roll, a 7 or an 11 automatically wins, and a 2, 3, or 12 automatically loses,");
-            System.out.println("and play is over. If a 4, 5, 6, 8, 9, or 10 are rolled on this first roll, that number becomes the point.");
-            System.out.println("3. The player continues to roll the two dice again until one of two things happens: either they roll the");
-            System.out.println("point from that first roll again, in which case they win; or they roll a 7, in which case they lose");
+        if (initiateGame.equalsIgnoreCase("No")){
+            System.out.println("Okay! Hope to see you soon!");
+        } else {
+            System.out.println("Do you need the instructions? (Yes/No)");
+            String instructions = in.next();
+            if (instructions.equalsIgnoreCase("Yes")){
+                System.out.println(" 1. A player rolls two six-sided dice and adds the numbers rolled together.");
+                System.out.println("2. On this first roll, a 7 or an 11 automatically wins, and a 2, 3, or 12 automatically loses,");
+                System.out.println("and play is over. If a 4, 5, 6, 8, 9, or 10 are rolled on this first roll, that number becomes the point.");
+                System.out.println("3. The player continues to roll the two dice again until one of two things happens: either they roll the");
+                System.out.println("point from that first roll again, in which case they win; or they roll a 7, in which case they lose");
+                System.out.println();
+            }
         }
         while (initiateGame.equalsIgnoreCase("Yes")){
             System.out.println("Press <Enter> to roll the dice");
-            String enter = in.next();
+            Scanner enterToRoll = new Scanner(System.in);
+            enterToRoll.nextLine();
             int roll = dice.dieRoll();
             if (roll == 7 || roll == 11){
                 System.out.println("You rolled a " +roll +"! Congrats, you win!");
@@ -31,14 +37,21 @@ public class Craps {
                 while (roll != 7 && point != roll || count == 0){
                     count ++;
                     System.out.println("You rolled a " +roll +". Press enter to roll again");
-                    String secondEnter = in.next();
-                    int secondRoll = dice.dieRoll();
-                    if (secondRoll == 7) {
-                        System.out.println("You rolled a" +roll +"! Sorry, you lose!");
-                    } else if (secondRoll == roll){
+                    enterToRoll.nextLine();
+                    roll = dice.dieRoll();
+                    if (roll == 7) {
+                        System.out.println("Point count:" +count);
+                        System.out.println("You rolled a " +roll +"! Sorry, you lose!");
+                    } else if (roll == point){
+                        System.out.println("Point count:" +count);
                         System.out.println("You rolled a " +roll +" again! Congrats, you win");
                     }
                 }
+            }
+            System.out.println("Game over! Would you like to play again?");
+            String response = in.next();
+            if (response.equalsIgnoreCase("No")){
+                break;
             }
         }
 
--- Die.java
+++ Die.java
@@ -1,8 +1,8 @@
 public class Die{
 
     public int dieRoll(){ //rolls the dice
-        int firstRoll = (int)Math.random()*6 + 1;
-        int secondRoll = (int)Math.random()*6 + 1;
+        int firstRoll = (int)(Math.random()*6) + 1;
+        int secondRoll = (int)(Math.random()*6) + 1;
         return firstRoll+secondRoll;
     }
 
